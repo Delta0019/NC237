1a61c5ed7b37d2e5e6f80c98c18b52f46925f00d
wenxuan <deltaleung3@gmail.com>
2025-07-13 16:43:28 +0800

    NC16884

--- NC16884.java
+++ NC16884.java
@@ -5,7 +5,7 @@ import java.io.PrintWriter;
 import java.io.StreamTokenizer;
 import java.util.HashMap;
 
-public class Main {
+public class NC16884 {
 
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static StreamTokenizer st = new StreamTokenizer(br);
