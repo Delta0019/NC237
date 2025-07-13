8e1a1e1f2d378eb318c88ee173009a1502c1c365
wenxuan <deltaleung3@gmail.com>
2025-07-13 12:39:09 +0800

    NC23803

--- NC23803.java
+++ NC23803.java
@@ -5,7 +5,7 @@ import java.io.PrintWriter;
 import java.io.StreamTokenizer;
 import java.util.HashMap;
 
-public class Main {
+public class NC23803 {
 
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static StreamTokenizer st = new StreamTokenizer(br);
@@ -33,6 +33,36 @@ public class Main {
     static HashMap<String, String> acient = new HashMap<>();
 
     public static void main(String[] args) throws Exception {
+        int n = nextInt(), m = nextInt();
+        for (int i = 0; i < n; ++i) {
+            String name = nextStr();
+            acient.put(name, name);
+        }
+        for (int i = 0; i < m; ++i) {
+            int opt = nextInt();
+            String p1 = nextStr(), p2 = nextStr();
+            if (opt == 1) {
+                merge(p1, p2);
+            } else {
+                OUT.println(find(p1) == find(p2) ? 1 : 0);
+            }
+        }
+        OUT.flush();
+    }
+
+    public static String find(String name) {
+        if (acient.get(name) == name) {
+            return name;
+        } else {
+            acient.put(name, find(acient.get(name)));
+            return acient.get(name);
+        }
+    }
+
+    public static void merge(String n1, String n2) {
+        if (acient.get(n1) != acient.get(n2)) {
+            acient.put(find(n1), find(n2));
+        }
     }
 
 }
