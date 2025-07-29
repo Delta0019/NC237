eccc13e541742a6db32a800ed869a096332f224c
wenxuan <deltaleung3@gmail.com>
2025-07-29 12:18:57 +0800

    NC16693

---
+++ MNC16693.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class MNC16693 {
+
+    static boolean[] dp = new boolean[20100];
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int v = input.nextInt();
+        int n = input.nextInt();
+        int[] things = new int[n];
+        for (int i = 0; i < n; ++i) {
+            things[i] = input.nextInt();
+        }
+        input.close();
+
+        dp[0] = true;
+        for (int i = 0; i < n; ++i) {
+            for (int j = v; j >= things[i]; --j) {
+                dp[j] = dp[j] || dp[j - things[i]];
+            }
+        }
+        for (int j = v; j >= 0; --j) {
+            if (dp[j]) {
+                System.out.println(v - j);
+                break;
+            }
+        }
+    }
+}
